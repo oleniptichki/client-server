851ab9219c04d3f4844ad88780392978af929caf
tatiana <tania_chel@list.ru>
2018-09-06 17:52:07 +0300

    fix draw4

--- serverfunc_oil_draw.py
+++ serverfunc_oil_draw.py
@@ -166,7 +166,7 @@ class oil_draw:
         print(massiv[-1])
         path=full_filename.rstrip(massiv[-1])
         print(path)
-        temp_filename = full_filename.rstrip('.txt') + '1.txt'
+        temp_filename = full_filename.replace('.txt', '1.txt')
         print("temp_filename=", temp_filename)
         return 4
         i=1
